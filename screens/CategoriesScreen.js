00e81b346982df040750aadf919960019e992d6b
Evelin Maripuu <evelin.marit@gmail.com>
2025-06-30 21:13:49 +0300

    Displaying Items in a Grid, p 94

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -4,16 +4,21 @@ import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
 function renderCategoryItem(itemData) {
-  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />;
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
 }
 
 function CategoriesScreen() {
   return (
-    <FlatList
+    <FlatList 
       data={CATEGORIES}
       keyExtractor={(item) => item.id}
       renderItem={renderCategoryItem}
+      numColumns={2}
+      key={4}      
     />
+    
   );
 }
 
